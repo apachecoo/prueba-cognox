55ba99382fc67bc34d4301edb886c07bbcfb77b2
Albert Pacheco <apachecoo110@gmail.com>
2022-04-26 20:33:45 -0500

    Primer commit

---
+++ app/Http/Controllers/Auth/LoginCognoxController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+class LoginCognoxController extends Controller
+{
+    
+    public function login(Request $request){
+
+        
+        $request->documento;
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
+//--antes
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+//--despues
+ Route::get('/login-cognox', [App\Http\Controllers\LoginCognoxController::class, 'index'])->name('login-cognox');
